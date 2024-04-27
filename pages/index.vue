f502ce5d21a3d0f3cebc589b245829fdb04557fe
Afkar-Saga <afkarsukmawanahmad@gmail.com>
2024-04-27 14:06:39 +0700

    index, log

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    
+    <h1>Selamat Datang</h1>
+    <h2>{{ nama_user }}</h2>
   </div>
 </template>
 
@@ -9,6 +10,12 @@ definePageMeta({
   layout: 'main',
   middleware: 'auth'
 })
+
+const supabase = useSupabaseClient()
+const user = useSupabaseUser()
+
+const { data: { nama_user }, error } = await supabase.from('users').select('nama_user').eq('id', user.value.id).maybeSingle()
+if (error) throw error
 </script>
 
 <style scoped>
--- pages/log.vue
+++ pages/log.vue
@@ -1,6 +1,30 @@
 <template>
   <div>
-    
+    <div class="title">Log Activity</div>
+    <div class="table">
+      <table>
+        <caption>Log</caption>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Username</th>
+            <th>Waktu</th>
+            <th>Aktivitas</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-if="pending">
+            <td class="loader" colspan="4"><Loader /></td>
+          </tr>
+          <tr v-for="(log, index) in logs" :key="log.id">
+            <th>{{ index + 1 }}</th>
+            <td>{{ log.users.username }}</td>
+            <td>{{ log.waktu.split('.')[0].replace('T', ' ') }}</td>
+            <td>{{ log.aktivitas }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -9,8 +33,19 @@ definePageMeta({
   layout: 'main',
   middleware: 'auth'
 })
+
+const supabase = useSupabaseClient()
+
+const { data: logs, pending } = useAsyncData('log', async () => {
+  const { data, error } = await supabase.from('log').select(`
+    *,
+    users ( username )
+  `)
+  if (error) throw error
+  return data
+})
 </script>
 
 <style scoped>
-
+@import url('~/assets/css/main.css');
 </style>
